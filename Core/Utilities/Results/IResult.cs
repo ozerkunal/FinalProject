3b4ca1e3247a0e400e85b08bb3c19138cc512188
Özer Kutlu ÜNAL <ozer@unal-group.com>
2021-02-13 19:13:17 +0300

    10 Şubat 2021

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    //Temel voidler için başlanıç
+    public interface IResult
+    {
+        bool Success { get; }
+        string Message { get; }
+    }
+}
--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -10,8 +10,8 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseSqlServer(@"Data Source = OZERUNALNB\SQLEXPRESS; Initial Catalog = NORTHWND; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
-            optionsBuilder.UseSqlServer(@"Data Source = OZERUNAL; Initial Catalog = NORTHWND; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+            optionsBuilder.UseSqlServer(@"Data Source = OZERUNALNB\SQLEXPRESS; Initial Catalog = NORTHWND; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+            //optionsBuilder.UseSqlServer(@"Data Source = OZERUNAL; Initial Catalog = NORTHWND; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
         }
 
         public DbSet<Product> Products { get; set; }
